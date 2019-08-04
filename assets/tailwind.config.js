d0445fe339a27d262e3eaab58bdf92c5f18f0d73
Jeff Triplett <jeff.triplett@gmail.com>
2019-08-03 23:27:38 -0500

    :fire: Removes blank line in tailwind config

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
       sans: ['"PT Sans"', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
       serif: ['"PT Serif"', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
       mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
-
       display: ['"PT Sans"', 'sans-serif'],
       body: ['"PT Serif"', 'sans-serif'],
     },
