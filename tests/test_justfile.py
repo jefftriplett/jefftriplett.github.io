fa108634b410e0447859a6c0d1c88de730902009
Jeff Triplett <jeff.triplett@gmail.com>
2021-12-29 10:45:18 -0600

    :green_heart: Adds basic tests for project

---
+++ tests/test_justfile.py
@@ -0,0 +1,42 @@
+import pytest
+import subprocess
+
+
+@pytest.fixture(scope="session")
+def justfile():
+    process = subprocess.Popen(
+        ["just", "--summary"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    stdout, _ = process.communicate()
+    output = stdout.decode("utf-8").strip()
+    yield output
+
+
+def test_bootstrap(justfile):
+    assert "bootstrap" in justfile.split()
+
+
+def test_cibuild(justfile):
+    assert "cibuild" in justfile.split()
+
+
+def test_console(justfile):
+    assert "console" in justfile.split()
+
+
+def test_server(justfile):
+    assert "server" in justfile.split()
+
+
+def test_setup(justfile):
+    assert "setup" in justfile.split()
+
+
+def test_test(justfile):
+    assert "test" in justfile.split()
+
+
+def test_update(justfile):
+    assert "update" in justfile.split()
