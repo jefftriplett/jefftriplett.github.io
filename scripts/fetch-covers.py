3f4fc18ea95713aabbbaa57f9d360ae98a6ce283
Jeff Triplett <jeff.triplett@gmail.com>
2024-10-12 10:35:40 -0500

    ✨ Adds fetch-covers command

---
+++ scripts/fetch-covers.py
@@ -0,0 +1,49 @@
+# /// script
+# requires-python = ">=3.10"
+# dependencies = [
+#     "bs4",
+#     "httpx",
+#     "pydantic",
+#     "python-frontmatter",
+#     "rich",
+#     "typer",
+# ]
+# ///
+from pathlib import Path
+
+import frontmatter
+import httpx
+import typer
+from bs4 import BeautifulSoup
+from pydantic import BaseModel
+from rich import print
+
+
+class FrontmatterInfo(BaseModel):
+    category: str = "Series"
+    cover: str
+    date: str  # datetime = datetime.now()
+    link: str
+    title: str
+
+
+def main(filename: str):
+    doc = Path(filename).read_text()
+    post = frontmatter.loads(doc)
+    url = post["link"]
+
+    with httpx.Client() as client:
+        response = client.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        # Extract title from og:title meta tag
+        image = soup.find("meta", property="og:image").get("content")
+        post["cover"] = image
+
+        output = frontmatter.dumps(post)
+        print(output)
+        Path(filename).write_text(output)
+
+
+if __name__ == "__main__":
+    typer.run(main)
